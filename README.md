9e46615d40d3fcb337b9c71c58d72d6d88403b5b
Jan-Erik Rediger <janerik@fnordig.de>
2017-07-17 21:02:03 +0200

    Start documentation
    That is seriously lacking at the moment

---
+++ README.md
@@ -0,0 +1,17 @@
+# libbpf - A convenient wrapper around the eBPF kernel API
+
+[![crates.io](http://meritbadge.herokuapp.com/libbpf)](https://crates.io/crates/libbpf)
+[![Build Status](https://travis-ci.org/badboy/libbpf-rs.svg?branch=master)](https://travis-ci.org/badboy/libbpf-rs)
+
+## Documentation
+
+[Documentation available online](https://docs.rs/libbpf).
+
+## Example
+
+```rust
+```
+
+## License
+
+MIT. See [LICENSE](LICENSE).
--- libbpf/src/lib.rs
+++ libbpf/src/lib.rs
@@ -1,3 +1,15 @@
+//! # libbpf - A convenient wrapper around the eBPF kernel API
+//!
+//! eBPF stands for Extended Berkeley Packet Filter.
+//! eBPF is a in-kernel virtual machine, which allows to safely execute user supplied programs in
+//! the kernel.
+//!
+//! Programs can be loaded into the kernel using a syscall.
+//! In addition, data with programs can be shared through BPF maps.
+//! These are also managed from the user side via a syscall.
+//!
+//! `libbpf` offers a convenient wrapper around these syscalls.
+
 extern crate libc;
 extern crate libbpf_sys;
 
